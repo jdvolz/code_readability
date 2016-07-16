683292ab8df4cd4432403520413ec9eab9e310b0
Joshua Volz <jvolz@customerlobby.com>
2016-07-16 15:41:54 -0700

    Simplifying respond method, adding a couple more helper methods

--- example.rb
+++ example.rb
@@ -7,38 +7,42 @@ post '/receive_sms/?' do
 end
 
 class SmsReceivedHandler
-
   def initialize(body, from)
     @body = body
     @from = from
   end
 
   def respond
-    if secret_id && is_integer_one_through_five(@body) #secret_id is not set, probably a bug
-      reply_message = successful_secret_count(secret_id.to_i, @body.to_i)
-
-    elsif is_integer_one_through_five(@body)
-
-      reply_message = "Sorry we couldn't match up the rating to a secret, that might because it's been more than 4 hours since you received last secret."
-    else
-      reply_message = create_caller_from_secret(@body, @from)
-    end
+    reply_message = create_reply_message(secret_id, @body, @from)
 
     twiml = send_twilio_response(reply_message)
     twiml.text
   end
 
-  def successful_secret_and_count(secret_id, body)
+  private
+
+  def create_reply_message(secret_id, body, from)
+    reply_message = if secret_id && is_integer_one_through_five(body) #secret_id is not set, probably a bug
+      successful_secret_customer_rating(secret_id, body.to_i)
+    elsif is_integer_one_through_five(body)
+      "Sorry we couldn't match up the rating to a secret, that might because it's been more than 4 hours since you received last secret."
+    else
+      create_caller_from_secret(body, from)
+    end
+
+    return reply_message
+  end
+
+  def successful_secret_and_customer_rating(secret_id, customer_rating)
     secret = Secret.get(secret_id)
-    rating_from_sms = body
-    rating = Rating.create(:score => rating_from_sms)
+    rating = Rating.create(:score => customer_rating)
     secret.ratings << rating
     secret.save
 
     return get_itpsss_facts
   end
 
-  def create_caller_from_secret(body, from  )
+  def create_caller_from_secret(body, from)
     return_secret = Secret.first(:offset => rand(Secret.count))
     session['secretid'] = return_secret.id
     secret_id = return_secret.id # secret_id set here, but never used again
