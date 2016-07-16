b5a5eff47c1d5f3d93a670ed3168cccfbd16d6e1
Joshua Volz <jvolz@customerlobby.com>
2016-07-16 15:27:41 -0700

    Separate it into a handler class.  My naming could use some work.

--- example.rb
+++ example.rb
@@ -1,37 +1,51 @@
-
-def is_integer_one_through_five(str)
-  str =~ /^[1-5]$/
-end
-
-def send_twilio_response(message)
-  Twilio::TwiML::Response.new {|r| r.Sms message }
-end
-
 post '/receive_sms/?' do
   body = params['Body']
+  from = params['From']
 
-  if secret_id && is_integer_one_through_five(body) #secret_id is not set, probably a bug
-    secret = Secret.get(secret_id.to_i)
-    rating_from_sms = body.to_i
-    rating = Rating.create(:score => rating_from_sms)
-    secret.ratings << rating
-    secret.save
+  handler = SmsReceivedHandler.new(body, from)
+  handler.respond
+end
 
-    reply_message = get_itpsss_facts
-  elsif is_integer_one_through_five(body)
-    reply_message = "Sorry we couldn't match up the rating to a secret, that might because it's been more than 4 hours since you received last secret."
-  else
-    return_secret = Secret.first(:offset => rand(Secret.count))
-    session['secretid'] = return_secret.id
-    secret_id = return_secret.id # secret_id set here, but never used again
+class SmsReceivedHandler
 
-    secret = Secret.create(:body => body, :created_at => Time.now)
-    caller = Caller.first_or_create(:from => params['From'])
-    caller.secrets << secret
-    caller.save
-
-    reply_message = return_secret.body
+  def initialize(body, from)
+    @body = body
+    @from = from
   end
-  twiml = send_twilio_response(reply_message)
-  twiml.text
+
+  def is_integer_one_through_five(str)
+    str =~ /^[1-5]$/
+  end
+
+  def send_twilio_response(message)
+    Twilio::TwiML::Response.new {|r| r.Sms message }
+  end
+
+  def respond
+    if secret_id && is_integer_one_through_five(@body) #secret_id is not set, probably a bug
+      reply_message = successful_secret_count(secret_id.to_i, @body.to_i)
+      secret = Secret.get(secret_id.to_i)
+      rating_from_sms = @body.to_i
+      rating = Rating.create(:score => rating_from_sms)
+      secret.ratings << rating
+      secret.save
+
+      reply_message = get_itpsss_facts
+    elsif is_integer_one_through_five(@body)
+      reply_message = "Sorry we couldn't match up the rating to a secret, that might because it's been more than 4 hours since you received last secret."
+    else
+      return_secret = Secret.first(:offset => rand(Secret.count))
+      session['secretid'] = return_secret.id
+      secret_id = return_secret.id # secret_id set here, but never used again
+
+      secret = Secret.create(:body => @body, :created_at => Time.now)
+      caller = Caller.first_or_create(:from => @from)
+      caller.secrets << secret
+      caller.save
+
+      reply_message = return_secret.body
+    end
+    twiml = send_twilio_response(reply_message)
+    twiml.text
+  end
 end
