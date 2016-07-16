1e3704b648ccc8fe286c3cbfa47aa9cbc08aa0b7
Joshua Volz <jvolz@customerlobby.com>
2016-07-16 15:20:29 -0700

    Extracted methods is_integer_one_thru_five, send_twilio_response
    - stopped using params['Body'] more than once
    - extracted twiml, twiml.text calls present in all if branches

--- example.rb
+++ example.rb
@@ -1,43 +1,37 @@
-post "/receive_sms/?" do
-  body = params["Body"]
 
-  reply_message = get_itpsss_facts
+def is_integer_one_through_five(str)
+  str =~ /^[1-5]$/
+end
 
-  if secret_id && body.length == 1 && body =~ /[12345]/
+def send_twilio_response(message)
+  Twilio::TwiML::Response.new {|r| r.Sms message }
+end
+
+post '/receive_sms/?' do
+  body = params['Body']
+
+  if secret_id && is_integer_one_through_five(body) #secret_id is not set, probably a bug
     secret = Secret.get(secret_id.to_i)
-    rating_from_sms = params['Body'].to_i
+    rating_from_sms = body.to_i
     rating = Rating.create(:score => rating_from_sms)
     secret.ratings << rating
     secret.save
 
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms reply_message
-    end
-    twiml.text
-
-  elsif body.length == 1 && body =~ /[12345]/
-
-    message = "Sorry we couldn't match up the rating to a secret, that might because it's been more
-              than 4 hours since you received last secret."
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms message
-    end
-    twiml.text
-
-  else # it must be a secret!
+    reply_message = get_itpsss_facts
+  elsif is_integer_one_through_five(body)
+    reply_message = "Sorry we couldn't match up the rating to a secret, that might because it's been more than 4 hours since you received last secret."
+  else
     return_secret = Secret.first(:offset => rand(Secret.count))
-    session["secretid"] = return_secret.id
-    secret_id = return_secret.id
+    session['secretid'] = return_secret.id
+    secret_id = return_secret.id # secret_id set here, but never used again
 
-
-    secret = Secret.create(:body => params['Body'], :created_at => Time.now)
+    secret = Secret.create(:body => body, :created_at => Time.now)
     caller = Caller.first_or_create(:from => params['From'])
     caller.secrets << secret
     caller.save
 
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms return_secret.body
-    end
-    twiml.text
+    reply_message = return_secret.body
   end
+  twiml = send_twilio_response(reply_message)
+  twiml.text
 end
