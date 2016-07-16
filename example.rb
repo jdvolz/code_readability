6c812aadf3cc313ca614ca3121269af21a81645a
Joshua Volz <jvolz@customerlobby.com>
2016-07-16 15:06:30 -0700

    change formatting and remove the puts statements

--- example.rb
+++ example.rb
@@ -1,35 +1,33 @@
 post "/receive_sms/?" do
-  puts "post receive_sms"
-    body = params["Body"]
+  body = params["Body"]
 
   reply_message = get_itpsss_facts
+
   if secret_id && body.length == 1 && body =~ /[12345]/
     secret = Secret.get(secret_id.to_i)
-    puts secret
     rating_from_sms = params['Body'].to_i
     rating = Rating.create(:score => rating_from_sms)
     secret.ratings << rating
     secret.save
 
     twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms reply_message
+      r.Sms reply_message
     end
     twiml.text
 
   elsif body.length == 1 && body =~ /[12345]/
+
     message = "Sorry we couldn't match up the rating to a secret, that might because it's been more
               than 4 hours since you received last secret."
     twiml = Twilio::TwiML::Response.new do |r|
       r.Sms message
-  end
-  twiml.text
+    end
+    twiml.text
 
   else # it must be a secret!
     return_secret = Secret.first(:offset => rand(Secret.count))
-    puts return_secret.inspect
     session["secretid"] = return_secret.id
     secret_id = return_secret.id
-    puts secret_id.inspect
 
 
     secret = Secret.create(:body => params['Body'], :created_at => Time.now)
@@ -39,11 +37,7 @@ post "/receive_sms/?" do
 
     twiml = Twilio::TwiML::Response.new do |r|
       r.Sms return_secret.body
-
-
-
-  end
-  twiml.text
     end
+    twiml.text
+  end
 end
-
